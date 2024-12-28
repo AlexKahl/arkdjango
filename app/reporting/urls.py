b77bb908aa4773976fc876d1693cef85a500b9f7
Alexander Kahl <alexander.r.kahl@gmail.com>
2024-12-28 17:16:11 +0100

    feat: base skeleton reporting app

---
+++ app/reporting/urls.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 22 09:22:25 2021
+@title: "file title goes here"
+@description: "file description"
+@details: "file description"
+@author: Alex Kahl
+
+"""
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+
+urlpatterns = [
+
+
+]
